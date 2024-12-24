ea4a3a31992126985f933d7a9404f3daa249d252
Abhishek Kumar <abhishekkumar63871@gmail.com>
2024-12-25 01:29:55 +0530

    Login Signup Add Css

--- react-app/src/components/Signup.jsx
+++ react-app/src/components/Signup.jsx
@@ -25,20 +25,33 @@ function Signup() {
   return (
     <div>
       <Header/>
-      welcome to signup page
-      <br></br>
-      USERNAME:
-      <input type="text" value={username} onChange={(e)=>{
+      <div class="d-flex align-items-center justify-content-center vh-100 bg-primary">
+    <div class="card p-4 shadow" >
+      <form>
+        <h2 class="text-center mb-4 text-primary">Welcome to SignUp Page</h2>
+       
+        <div class="form-outline mb-4">
+          <label class="form-label" for="form2Example1">User Name</label>
+          <input type="text" class="form-control"  placeholder="Enter your User Name"  value={username} onChange={(e)=>{
         setusername(e.target.value)
       }}/>
-      <br></br>
-      PASSWORD:
-      <input type="text" value={password} onChange={(e)=>{
-        setpassword(e.target.value)
-      }}/>
-      <br></br>
-      <button onClick={handleApi}>Signup</button>
-      <Link to="/Loginn">LOGIN</Link>
+        </div>
+       
+        <div class="form-outline mb-4">
+          <label class="form-label" for="form2Example2">Password</label>
+          <input type="password"  class="form-control" placeholder="Enter your password" value={password} onChange={(e)=>{
+            setpassword(e.target.value)
+          }} />
+        </div>
+       
+        <div class="d-flex justify-content-between">
+          <button class="btn btn-primary w-45  " onClick={handleApi}>SIGNUP</button>
+          <Link type="button" class="btn btn-success w-45" to="/Loginn" >Login</Link>
+         
+        </div>
+      </form>
+    </div>
+  </div>
       
     </div>
   )
